a9c798f643c123416cacc8297de4ed0cbda0c94e
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-10 16:17:13 -0300

    Requsiite 2 done

--- src/index.js
+++ src/index.js
@@ -21,3 +21,11 @@ app.get('/talker', async (_req, res) => {
   const talkers = await readingTalkers();
   res.status(200).json(talkers);
 });
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readingTalkers();
+  const data = talkers.find((e) => e.id === Number(id));
+  if (!data) res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(data);
+});
